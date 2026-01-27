cb3d809ad594d7ba187ec0cb70b4060f4bec9b01
devrimkomur1212-byte <devrim.komur1212@gmail.com>
2026-01-27 20:45:21 +0000

    Issue sFIxed

--- wikirun/next.config.ts
+++ wikirun/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['upload.wikimedia.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+    ],
   },
   experimental: {
     serverActions: {
